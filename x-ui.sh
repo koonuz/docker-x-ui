13ff4cb8e8dfa97d70df5a9a1e12444964f78222
koonuz <42229875+koonuz@users.noreply.github.com>
2022-07-29 15:12:30 +0800

    Update x-ui.sh

--- x-ui.sh
+++ x-ui.sh
@@ -64,7 +64,7 @@ reset_config() {
 check_config() {
     info=$(/usr/local/x-ui/x-ui setting -show true)
     if [[ $? != 0 ]]; then
-        echo -n "get current settings error,please check logs"
+        echo -n "无法获取当前面板设置，请检查日志"
         show_menu
     fi
     echo -e "${info}"
